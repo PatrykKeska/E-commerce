34e474d4f400110364294f3e92d52c73ebb95102
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-28 20:42:36 +0200

    - Kids category page created

---
+++ src/pages/kids/index.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import { Nav } from '../../layouts/nav';
+
+export class Kids extends Component {
+  render() {
+    return (
+      <>
+        <Nav />
+        <section>this is kids page</section>
+      </>
+    );
+  }
+}
