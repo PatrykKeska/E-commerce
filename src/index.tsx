220baa5e04bfa4077d0a5a4152eab6121d48ae63
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-28 20:36:37 +0200

    - apply basic reset on index.scss file - import routing to index.tsx file.

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import {  RouterProvider } from 'react-router-dom';
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
-import { MainPage } from './pages/main/mainPage';
+import {router} from './setup/routes-manager';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainPage />,
-  },
-    {path:'/products', element:<App/>}
-]);
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
