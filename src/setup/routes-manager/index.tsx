d65d333f506909e23a095c0e42de6e87ff4b069a
PatrykKeska <PatrykKeska4@gmail.com>
2022-11-07 14:48:19 +0100

    - Changed routes path for pages and details of product page

--- src/setup/routes-manager/index.tsx
+++ src/setup/routes-manager/index.tsx
@@ -1,22 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { MainPage } from '../../pages/main/mainPage';
-import { All } from '../../pages/All';
-import { Tech } from '../../pages/Tech';
 import React from 'react';
-import Clothes from '../../pages/Clothes';
-import {ProductDetails} from '../../pages/Product-details';
+import { ProductDetailsPage } from '../../pages/Product-details-page';
+import AllCategoryPage from '../../pages/AllCategoryPage';
+import TechCategoryPage from '../../pages/TechCategoryPage';
+import ClothesCategoryPage from '../../pages/ClothesCategoryPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MainPage />,
   },
-  { path: '/all', element: <All /> },
-  { path: '/all/:id', element: <ProductDetails /> },
-  { path: '/clothes/', element: <Clothes /> },
-  { path: '/clothes/:id', element: <ProductDetails /> },
-  { path: '/tech', element: <Tech /> },
-  { path: '/tech/:id', element: <ProductDetails /> },
+  { path: '/all', element: <AllCategoryPage /> },
+  { path: '/all/:id', element: <ProductDetailsPage /> },
+  { path: '/clothes/', element: <ClothesCategoryPage /> },
+  { path: '/clothes/:id', element: <ProductDetailsPage /> },
+  { path: '/tech', element: <TechCategoryPage /> },
+  { path: '/tech/:id', element: <ProductDetailsPage /> },
 ]);
 
 export { router };
