d0f4c4c6427b319dd622a30d927e2be5c36743ea
PatrykKeska <PatrykKeska4@gmail.com>
2022-11-09 19:56:55 +0100

    clean up in Store config file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { currencySlice } from './features/currency/currency-slice';
-import {productDetailsSlice} from './features/product-details/product-details-slice';
+import { productDetailsSlice } from './features/product-details/product-details-slice';
 
 export const store = configureStore({
   reducer: {
     currency: currencySlice.reducer,
-    productDetails: productDetailsSlice.reducer
+    productDetails: productDetailsSlice.reducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type SliceDispatch = typeof  store.dispatch;
+export type SliceDispatch = typeof store.dispatch;
