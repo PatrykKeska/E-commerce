b8c8b4bbc3a9396d03c10d94c32fc02e7ebc0e42
PatrykKeska <PatrykKeska4@gmail.com>
2022-11-13 17:13:24 +0100

    => updated product-details-slice.ts by new values needed

--- src/store/features/product-details/product-details-slice.ts
+++ src/store/features/product-details/product-details-slice.ts
@@ -1,17 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export interface productDetailsState {
-  size: string;
-  color: string;
+  size?: string;
+  color?: string;
   price: number;
-  capacity: string;
+  allPrices: [];
+  name: string;
+  brand: string;
+  id:string;
+  gallery: string[];
+  allColors?:[],
+  allSizes?:[],
 }
 
 const initialState: productDetailsState = {
   size: '',
   color: '',
   price: 0,
-  capacity: '',
+  name:'',
+  brand:'',
+  id:'',
+  gallery:[],
+  allPrices:[],
+  allColors:[],
+  allSizes:[],
 };
 
 interface setColorDetails {
@@ -26,6 +38,28 @@ interface setCapacityDetails {
 interface setPriceDetails {
   payload: number;
 }
+interface setNameDetails {
+  payload: string
+}
+interface setBrandDetails {
+  payload: string
+}
+interface setIdDetails{
+  payload: string;
+}
+interface setGalleryDetails{
+  payload: string[];
+}
+interface setAllPrices{
+  payload: [];
+}
+interface setAllColors {
+  payload :[]
+};
+interface setAllSizes {
+  payload :[]
+}
+
 
 export const productDetailsSlice = createSlice({
   name: 'productDetails',
@@ -40,11 +74,30 @@ export const productDetailsSlice = createSlice({
     setProductSize: (state: productDetailsState, action: setSizeDetails) => {
       state.size = action.payload;
     },
-    setCapacity: (state: productDetailsState, action: setCapacityDetails) => {
-      state.capacity = action.payload;
+
+    setProductName: (state: productDetailsState, action: setNameDetails) => {
+      state.name = action.payload;
+    },
+    setBrandName: (state: productDetailsState, action: setBrandDetails) => {
+      state.brand = action.payload;
+    },
+    setProductId: (state: productDetailsState, action: setIdDetails) => {
+      state.id = action.payload;
+    },
+    setProductGallery: (state: productDetailsState, action: setGalleryDetails) => {
+      state.gallery = action.payload;
+    },
+    setProductAllPrices: (state: productDetailsState, action: setAllPrices) => {
+      state.allPrices = [...action.payload];
+    },
+    setProductAllColors: (state: productDetailsState, action: setAllColors) => {
+      state.allColors = [...action.payload];
+    },
+    setProductAllSizes: (state: productDetailsState, action: setAllSizes) => {
+      state.allSizes = [...action.payload];
     },
   },
 });
 
-export const { setProductColor, setProductSize, setCapacity, setProductPrice } =
+export const { setProductColor, setProductSize, setProductPrice,setProductName,setProductId,setBrandName, setProductGallery, setProductAllPrices, setProductAllColors, setProductAllSizes } =
   productDetailsSlice.actions;
