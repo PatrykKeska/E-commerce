9a6b62afcdbecb8e6a01164f0fb455e0a1ce617d
PatrykKeska <PatrykKeska4@gmail.com>
2022-11-25 09:22:23 +0100

    - In cart button added : - span to display number of products in cart - method to open a cart component - styles for all of above

--- src/layouts/nav/components/basketButton/index.tsx
+++ src/layouts/nav/components/basketButton/index.tsx
@@ -1,14 +1,28 @@
 import { Component } from 'react';
 import { BasketIcon } from '../../../../assets/svg/BasketIcon';
 import './style.scss';
-class BasketButton extends Component {
+import { ReduxHOC } from '../../../../pages/common/components/ReduxHOC';
+import { Props } from './types';
+import { handleCartState } from '../../../../store/features/basket/basket-slice';
+class BasketButtonC extends Component<Props> {
+  handleCart = (state: boolean) => {
+    const {  dispatch } = this.props;
+    dispatch(handleCartState(state));
+  };
   render() {
+    const { basketSelector } = this.props;
+    const {items} = basketSelector
     return (
-      <button className='button'>
-        <BasketIcon />
-      </button>
+        <button className='button'>
+          <BasketIcon
+            onClick={() => this.handleCart(true)}
+          />
+            {items.length > 0 && <span className='button__items-in-cart'>{items.length}</span> }
+
+        </button>
     );
   }
 }
 
+const BasketButton = ReduxHOC(BasketButtonC);
 export { BasketButton };
