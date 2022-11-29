991dee206a75b06ebf2f72bd068cbe015d07455f
PatrykKeska <PatrykKeska4@gmail.com>
2022-11-29 10:13:25 +0100

    - styles for checkout modal added - types added for checkoutmodal

---
+++ src/pages/Cart/components/checkout-modal/types.ts
@@ -0,0 +1,8 @@
+import { Basket } from '../../../../store/features/basket/basket-slice';
+import { Dispatch } from 'redux';
+
+export interface Props {
+  basketSelector: Basket;
+  currency: { value: string };
+  dispatch: Dispatch;
+}
---
+++ src/pages/common/components/HooksAccessComponent.tsx
@@ -0,0 +1,33 @@
+import React, { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../store';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const HooksAccessComponent = (Component: React.ComponentType<any>) => {
+  return function hooks(props) {
+    const dispatch = useDispatch();
+    const param = useParams();
+    const selector = useSelector((store: RootState) => store.productDetails);
+    const basketSelector = useSelector((store: RootState) => store.basket);
+    const navi = useNavigate();
+    const modalSelector = useSelector((store: RootState) => store.modal);
+    const currency = useSelector((store: RootState) => store.currency);
+    const ref = useRef(null);
+
+    return (
+      <Component
+        {...props}
+        dispatch={dispatch}
+        selector={selector}
+        currency={currency}
+        useParams={param}
+        navigate={navi}
+        basketSelector={basketSelector}
+        modalSelector={modalSelector}
+        useRef={ref}
+      />
+    );
+  };
+};
+
+export { HooksAccessComponent };
